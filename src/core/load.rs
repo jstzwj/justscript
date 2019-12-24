0a8816025f2e18b48a5dd8b9e683ff1da42941d2
jstzwj <1103870790@qq.com>
2019-12-24 20:06:16 +0800

    move syntax to another repo

--- src/core/load.rs
+++ src/core/load.rs
@@ -1,13 +1,9 @@
-use std::path::Path;
 use std::fs;
+use std::path::Path;
 
-pub fn load_from_string(code:&str) {
+pub fn load_from_string(code: &str) {}
 
-}
-
-
-pub fn load_from_file(path:&str) {
-    let contents = fs::read_to_string(path)
-        .expect("Something went wrong reading the file");
+pub fn load_from_file(path: &str) {
+    let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
     load_from_string(&contents);
 }
--- src/syntax/ast/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod punctuator;
-pub mod token;
