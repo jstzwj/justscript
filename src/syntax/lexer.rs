176172599328370b90a60ecaf93c91ceeb5f8e6d
JasonWang <1103870790@qq.com>
2020-01-15 23:59:09 +0800

    remove unnused files

--- src/syntax/lexer.rs
+++ src/syntax/lexer.rs
@@ -6,43 +6,6 @@ use std::str::Chars;
 use std::error;
 use std::fmt;
 
-pub struct LexerError {
-    code: i32,
-    message: String,
-}
-
-impl LexerError {
-    fn new(msg: &str) -> Self {
-        Self {
-            code: 0,
-            message: msg.to_string(),
-        }
-    }
-}
-
-impl fmt::Display for LexerError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl fmt::Debug for LexerError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{{ file: {}, line: {} }}", file!(), line!()) // programmer-facing output
-    }
-}
-
-impl error::Error for LexerError {
-    fn description(&self) -> &str {
-        &self.message
-    }
-
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        // The lower-level source of this error, if any.
-        None
-    }
-}
-
 /// Parses the first token from the provided input string.
 pub fn first_token(input: &str) -> Token {
     debug_assert!(!input.is_empty());
